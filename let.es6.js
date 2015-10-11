4ffabed2c5b8f94f44b3f9dc8868c6f202eba578
kyokutyo <haruta@kyokutyo.com>
2015-10-12 02:17:39 +0900

    より複雑なブロックスコープ変数

--- let.es6.js
+++ let.es6.js
@@ -3,5 +3,11 @@
 var x = 1;
 if (true) {
     let x = 3;
+    if (true) {
+        let x = 5;
+        if (true) {
+            let x = 7;
+        }
+    }
 }
 console.log(x); // 1
--- let.js
+++ let.js
@@ -3,5 +3,11 @@
 var x = 1;
 if (true) {
     var _x = 3;
+    if (true) {
+        var _x2 = 5;
+        if (true) {
+            var _x3 = 7;
+        }
+    }
 }
 console.log(x); // 1
